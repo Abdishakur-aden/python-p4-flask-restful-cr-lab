d82b9ffe2e6f0acc71382a7d79c6c056ff72f0b8
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-07-06 14:42:06 +0300

    completed lab

--- server/models.py
+++ server/models.py
@@ -6,4 +6,9 @@ db = SQLAlchemy()
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+    serialize_only = ('id', 'name', 'image', 'price')
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
